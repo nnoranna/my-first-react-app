413558f4ff1f005e0d3de56b470fef25e9f8c378
nnoranna <anagy.nora.anna@gmail.com>
2022-04-08 09:44:21 +0200

    Practice import-export

---
+++ src/App2.js
@@ -0,0 +1,13 @@
+import logo2 from './codecoollogo.png';
+import './App.css';
+
+
+function Footer() {
+  return (
+    <div className="footer">
+         <img src={logo2} className="cc-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Footer;
