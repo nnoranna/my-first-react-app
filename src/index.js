91bac0301f5eeb1af9191071f387e7fbbc8c1e33
nnoranna <anagy.nora.anna@gmail.com>
2022-04-08 14:11:00 +0200

    Delete unnecessary rows in index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Footer from './App2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
